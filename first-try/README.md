d8afe56a158ddf7a93d22849c558bd7dd4a2844b
Richard <nabadjarichard@gmail.com>
2022-07-08 09:10:36 +0800

    Mulah technologies front-end Assessment

---
+++ first-try/README.md
@@ -0,0 +1,7 @@
+# Bob chatbot
+In conversation, his responses are very limited:
+-Bob answers 'Sure.'     if you ask him a question                                  (by typing "how", "who", "when", "where", or "what").
+-He answers 'Whoa, chill out!'  if you yell at him                                  (by typing "yell" or "hee").
+-He retorts 'Calm down, I know what I'm doing!' if you yell a question at him       (by typing "yell who", "yell how", "yell when", "yell what" or "yell where").
+-He says 'Fine. Be that way!'   if you address him without actually saying anything (by typing "bob").
+-He answers 'Whatever.' to anything else.
---
+++ first-try/constants.js
@@ -0,0 +1,28 @@
+// Options the user could type in
+const prompts = [
+ 
+  ["how are you", "how is life", "how are things", "how", "when"],
+  ["what are you doing", "what is going on", "what is up", "what"],
+  ["who are you", "who", "where"],
+  ["yell","he", "hee", "heee", "heeee" ],
+  ["yell who are you", "yell who", "yell how","yell when", "yell where", "yell what", "hee do you know me"],
+  ["bob"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  
+  ["sure"],
+  ["sure"],
+  ["sure"],
+  ["Whoa, chill out!"],
+  ["Calm down, I know what I'm doing!"],
+  ["Fine. Be that way!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "whatever"
+]
